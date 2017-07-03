c08af25ded38c63e13c5b093106d0c6b62779588
hanguang <hanguang@pyyx.com>
2017-07-03 18:08:14 +0800

    实现字符串排序

---
+++ 002.php
@@ -0,0 +1,34 @@
+<?php
+
+/** 根据首字母排序,忽略大小写
+ * @param array $arr
+ * @param string $sort_flag 'desc' 倒序，'asc'正序
+ * @return array|bool
+ */
+function strSort(array $arr, $sort_flag = 'asc')
+{
+    if (!$arr) {
+        return false;
+    }
+    $len = count($arr);
+    for ($i = 1; $i < $len; $i++) {
+        for ($j = 0; $j < $len - $i ; $j++) {
+            if ($sort_flag == 'desc') {
+                if (ord(strtolower($arr[$j + 1]{0})) > ord(strtolower($arr[$j]{0}))) {
+                    $tmp = $arr[$j + 1];
+                    $arr[$j + 1] = $arr[$j];
+                    $arr[$j] = $tmp;
+                }
+            } else {
+                if (ord(strtolower($arr[$j]{0})) > ord(strtolower($arr[$j + 1]{0}))) {
+                    $tmp = $arr[$j + 1];
+                    $arr[$j + 1] = $arr[$j];
+                    $arr[$j] = $tmp;
+                }
+            }
+        }
+    }
+    return $arr;
+}
+$arr = ['ant', 'old', 'Free', 'bold', 'weight', 'X', 'Line', 'tea', 'over', 'pink', 'yea', 'zoo', 'distance'];
+var_dump(strSort($arr, 'desc'));
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### PHP实现一些算法记录
 
-001 [数字排序](https://github.com/ahcometrue/algorithm/001.php)
+001 [数字排序](https://github.com/ahcometrue/algorithm/blob/master/001.php)
+
+002 [字符串排序](https://github.com/ahcometrue/algorithm/blob/master/002.php)
